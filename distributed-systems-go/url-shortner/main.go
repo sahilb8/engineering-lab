5d3a03202dc1058440c01ef48ce2e435eb26faef
sahilb8 <sahilbanastarkar7184@gmail.com>
2026-03-15 15:15:30 +0530

    added Store interface

--- distributed-systems-go/url-shortner/main.go
+++ distributed-systems-go/url-shortner/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"log"
+	"math/big"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
+
+type Store interface {
+	Save(code, url string) error
+	Lookup(code string) (string, bool, error)
+}
+
+type App struct {
+	store Store
+}
 
 type urlShortenRequest struct {
 	URL string `json:"url"`
@@ -16,6 +28,41 @@ type urlShortenRequest struct {
 type urlShortResponse struct {
 	ShortURL string `json:"short_url"`
 }
+type inMemoryStore struct {
+	mu   sync.RWMutex
+	data map[string]string
+}
+
+func generateShortKey() (string, error) {
+	const n = 7
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		result[i] = charset[num.Int64()]
+	}
+	return string(result), nil
+}
+
+func (s *inMemoryStore) Save(code, url string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.data[code] = url
+
+	return nil
+}
+
+func (s *inMemoryStore) Lookup(code string) (string, bool, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	val, exists := s.data[code]
+	return val, exists, nil
+}
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -23,7 +70,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"status": "ok"}`))
 }
 
-func shortenHandler(w http.ResponseWriter, r *http.Request) {
+func (app *App) shorten(w http.ResponseWriter, r *http.Request) {
 	var req urlShortenRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
 
@@ -35,17 +82,64 @@ func shortenHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "URL empty", http.StatusBadRequest)
 		return
 	}
-	resp := urlShortResponse{
-		ShortURL: req.URL,
+
+	var shortKey string
+	var shortKeyError error
+	for i := 0; i < 5; i++ {
+		shortKey, shortKeyError = generateShortKey()
+		if shortKeyError != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if _, exists, _ := app.store.Lookup(shortKey); !exists {
+			app.store.Save(shortKey, req.URL)
+			break
+		} else {
+			shortKey = ""
+		}
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(resp)
+
+	if shortKey != "" {
+		resp := urlShortResponse{
+			ShortURL: "http://localhost:8000/" + shortKey,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(resp)
+	} else {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+
+}
+
+func (app *App) redirectHandler(w http.ResponseWriter, r *http.Request) {
+	// "code" matches the {code} in the route pattern
+	code := r.PathValue("code")
+
+	if code == "" {
+		http.Error(w, "code missing", http.StatusBadRequest)
+		return
+	}
+
+	longURL, exists, _ := app.store.Lookup(code)
+	if exists {
+		http.Redirect(w, r, longURL, http.StatusFound)
+	} else {
+		http.Error(w, "Short URL not found", http.StatusNotFound)
+		return
+	}
 }
 
 func main() {
+	var memoryStore Store = &inMemoryStore{data: make(map[string]string)}
+
+	app := &App{
+		store: memoryStore,
+	}
+
 	http.HandleFunc("GET /health", healthHandler)
-	http.HandleFunc("POST /shorten", shortenHandler)
+	http.HandleFunc("POST /shorten", app.shorten)
+	http.HandleFunc("GET /{code}", app.redirectHandler)
 
 	err := godotenv.Load()
 	if err != nil {
