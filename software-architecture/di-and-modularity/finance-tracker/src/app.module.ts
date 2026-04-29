bd898545ea3a8b622a2be61076bd1fa6b0eccd9d
sahilb8 <sahilbanastarkar7184@gmail.com>
2026-04-29 15:28:32 +0530

    initialised all the modules

--- software-architecture/di-and-modularity/finance-tracker/src/app.module.ts
+++ software-architecture/di-and-modularity/finance-tracker/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { HouseholdsModule } from './households/households.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    HouseholdsModule,
+    AccountsModule,
+    TransactionsModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
